2adf55d851a5160c6f1f9d05bda320fa48f9efa6
hisomura <hisomura.noreply@gmail.com>
2020-09-09 08:16:31 +0900

    無理やり型を付けて@ts-ignoreを消した。 https://redux.js.org/tutorials/essentials/part-2-app-structure 読了。

--- src/features/counter/counterSlice.ts
+++ src/features/counter/counterSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import {createSlice} from '@reduxjs/toolkit';
 
 export const counterSlice = createSlice({
   name: 'counter',
@@ -16,13 +16,13 @@ export const counterSlice = createSlice({
     decrement: state => {
       state.value -= 1;
     },
-    incrementByAmount: (state, action) => {
+    incrementByAmount: (state, action: { payload: number }) => {
       state.value += action.payload;
     },
   },
 });
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const {increment, decrement, incrementByAmount} = counterSlice.actions;
 
 
 // The function below is called a thunk and allows us to perform async logic. It
@@ -38,7 +38,6 @@ export const incrementAsync = (amount: number) => (dispatch: CallableFunction) =
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state) => state.counter.value)`
-// @ts-ignore
-export const selectCount = state => state.counter.value;
+export const selectCount = (state: { counter: { value: number } }) => state.counter.value;
 
 export default counterSlice.reducer;
